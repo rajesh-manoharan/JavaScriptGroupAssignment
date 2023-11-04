43ad930e232d42bc3adf5b00a426855ff0965b44
rajesh-manoharan <111693523+rajesh-manoharan@users.noreply.github.com>
2023-11-05 02:52:49 +0530

    Add files via upload
    Resume search completed app

--- resume-item/view/resume-item-view.js
+++ resume-item/view/resume-item-view.js
@@ -2,10 +2,67 @@ import { resumeItem } from "../model/resume-item.js";
 
 class JobItemView {
 
-    constructor(item) {
-        this.item = item;
-        this.object = document.getElementById(this.item);
+    constructor() {
+        this.itemNumber = 0;
     }
+
+    displayResumeDetails(allJobItems) {
+
+        this.parentObject = document.getElementById("personal");
+        this.object = document.createElement("div");
+        this.object.setAttribute("id", "personalSection");
+        this.parentObject.appendChild(this.object);
+        this.buildResume(allJobItems[this.itemNumber]["personal"]);
+
+        this.parentObject = document.getElementById("education");
+        this.object = document.createElement("div");
+        this.object.setAttribute("id", "educationSection");
+        this.parentObject.appendChild(this.object);
+        this.buildResume(allJobItems[this.itemNumber]["education"]);
+
+        this.parentObject = document.getElementById("work");
+        this.object = document.createElement("div");
+        this.object.setAttribute("id", "workSection");
+        this.parentObject.appendChild(this.object);
+        this.buildResume(allJobItems[this.itemNumber]["work"]);
+
+        this.parentObject = document.getElementById("projects");
+        this.object = document.createElement("div");
+        this.object.setAttribute("id", "projectsSection");
+        this.parentObject.appendChild(this.object);
+        this.buildResume(allJobItems[this.itemNumber]["projects"]);
+
+        this.parentObject = document.getElementById("Internship");
+        this.object = document.createElement("div");
+        this.object.setAttribute("id", "InternshipSection");
+        this.parentObject.appendChild(this.object);
+        this.buildResume(allJobItems[this.itemNumber]["internship"]);
+
+        this.parentObject = document.getElementById("achievements");
+        this.object = document.createElement("div");
+        this.object.setAttribute("id", "achievementsSection");
+        this.parentObject.appendChild(this.object);
+        this.buildResume(allJobItems[this.itemNumber]["achievements"]);
+
+        this.parentObject = document.getElementById("basics");
+        this.object = document.createElement("div");
+        this.object.setAttribute("id", "basicsSection");
+        this.parentObject.appendChild(this.object);
+        this.buildResume(allJobItems[this.itemNumber]["basics"]);
+
+        this.parentObject = document.getElementById("skills");
+        this.object = document.createElement("div");
+        this.object.setAttribute("id", "skillsSection");
+        this.parentObject.appendChild(this.object);
+        this.buildResume(allJobItems[this.itemNumber]["skills"]);
+
+        this.parentObject = document.getElementById("interests");
+        this.object = document.createElement("div");
+        this.object.setAttribute("id", "interestsSection");
+        this.parentObject.appendChild(this.object);
+        this.buildResume(allJobItems[this.itemNumber]["interests"]);
+    }
+
     buildResume(jsonObj) {
         for (let jsonLoop in jsonObj) {
             if (!!jsonObj[jsonLoop] && typeof (jsonObj[jsonLoop]) == "object") {
@@ -23,6 +80,51 @@ class JobItemView {
             }
         }
     }
+
+    displayPrevButton(prevBtn, totalCount) {
+        prevBtn.disabled = true;
+        if (this.itemNumber > 0 && this.itemNumber <= (totalCount - 1)) {
+            prevBtn.disabled = false;
+        }
+    }
+
+    displayNextButton(nextBtn, totalCount) {
+        nextBtn.disabled = true
+        if (this.itemNumber >= 0 && this.itemNumber < (totalCount - 1)) {
+            nextBtn.disabled = false;
+        }
+    }
+
+    resetResume() {
+        if (document.getElementById('personalSection')) {
+            document.getElementById("personalSection").remove();
+            document.getElementById("educationSection").remove();
+            document.getElementById("workSection").remove();
+            document.getElementById("projectsSection").remove();
+            document.getElementById("InternshipSection").remove();
+            document.getElementById("achievementsSection").remove();
+            document.getElementById("basicsSection").remove();
+            document.getElementById("skillsSection").remove();
+            document.getElementById("interestsSection").remove();
+        }
+        console.log('Reset button function', this.itemNumber);
+    }
+
+    resetResumeCounter() {
+        this.itemNumber = 0;
+    }
+
+
+    clickPrevButton() {
+        this.itemNumber--;
+        console.log('Prev button function', this.itemNumber);
+    };
+
+    clickNextButton() {
+        this.itemNumber++;
+        console.log('Next button function', this.itemNumber);
+    }
+
 }
 
 export { JobItemView };
--- view/resume-app-view.js
+++ view/resume-app-view.js
@@ -1,7 +1,57 @@
+import { JobCategory } from '../job-category/model/job-category.js';
+import { JobCategoryUtils } from '../job-category/model/job-category-utility.js'
+import { JobItemView } from "../resume-item/view/resume-item-view.js";
 
 class ResumeAppView {
-    constructor(jobCategories) {
-        this.jobCategories = jobCategories;
+    constructor() {
+        this.prevBtn = document.getElementById("prevBtn");
+        this.searchBox = document.getElementById("searchbox");
+        this.nextBtn = document.getElementById("nextBtn");
+        this.resetBtn = document.getElementById("resetBtn");
+        this.jobItemView = new JobItemView();
+        this.allJobItems = null;
+    }
+
+    displaySearchResume() {
+        this.searchBox.addEventListener("keypress", (event) => {
+            if (event.key === "Enter") {
+                const inputJobCategory = new JobCategory(this.searchBox.value);
+                this.allJobItems = JobCategoryUtils.getAllJobItems(inputJobCategory);
+
+                if (this.allJobItems.length === 0) {
+                    alert('No application');
+                } else {
+                    this.searchBox.disabled = true;
+                    this.jobItemView.displayResumeDetails(this.allJobItems);
+                    this.jobItemView.displayPrevButton(this.prevBtn, this.allJobItems.length);
+                    this.jobItemView.displayNextButton(this.nextBtn, this.allJobItems.length);
+                }
+            }
+        })
+
+
+        this.prevBtn.addEventListener("click", (event) => {
+            this.jobItemView.clickPrevButton();
+            this.jobItemView.resetResume();
+            this.jobItemView.displayResumeDetails(this.allJobItems);
+            this.jobItemView.displayPrevButton(this.prevBtn, this.allJobItems.length);
+            this.jobItemView.displayNextButton(this.nextBtn, this.allJobItems.length);
+        })
+
+        this.nextBtn.addEventListener("click", (event) => {
+            this.jobItemView.clickNextButton();
+            this.jobItemView.resetResume();
+            this.jobItemView.displayResumeDetails(this.allJobItems);
+            this.jobItemView.displayPrevButton(this.prevBtn, this.allJobItems.length);
+            this.jobItemView.displayNextButton(this.nextBtn, this.allJobItems.length);
+        })
+
+        this.resetBtn.addEventListener("click", (event) => {
+            this.jobItemView.resetResumeCounter();
+            this.jobItemView.resetResume();
+            this.searchBox.disabled = false;
+            this.searchBox.value = '';
+        })
     }
 }
-export {ResumeAppView };
+export { ResumeAppView };
